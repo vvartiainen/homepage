eadc7391d689b86b878ad5692cf278907c38087c
Ville Vartiainen <vvarti@gmail.com>
2024-03-02 21:31:59 +0200

    Fix structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,26 @@
 <script>
 	import { Separator } from '$lib/components/ui/separator';
+	import { Button } from '$lib/components/ui/button';
 </script>
 
 <svelte:head>
 	<title>Portfolio</title>
-	<meta name="Portfolio" content="Portfolio" />
+	<meta name="Portfolio - Ville Vartiainen" content="Portfolio - Ville Vartiainen" />
 </svelte:head>
 
-<header class="mb-5 mt-5 gap-x-5 text-center font-mono font-bold">
-	<p class="mb-5 text-5xl font-bold">vartiainen.io</p>
-	<a href="/">Home</a>
-	<a href="/">Projects</a>
+<header class="mb-5 mt-10 gap-x-5 text-center font-mono font-bold">
+	<p class="mb-5 text-4xl font-bold">vartiainen.io</p>
 </header>
+
+<nav class="justify-left mb-5 flex pl-12">
+	<a href="/"><Button variant="link" class="underline-offset-8">Home</Button></a>
+	<a href="/"><Button variant="link" class="underline-offset-8">CV</Button></a>
+</nav>
 
 <Separator class="my-4" />
 
 <section class="mb-10 flex items-center justify-center font-mono">
-	<div class="w-1/2 gap-5 space-y-8 p-20">
+	<div class="w-3/5 justify-center gap-5 space-y-8 p-20">
 		<p>
 			Lorem ipsum dolor sit amet. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
 			aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
@@ -31,17 +35,19 @@
 			beatae vitae dicta sunt explicabo.
 		</p>
 	</div>
-	<div class="flex w-1/3 items-center justify-center">
-		<div>
-			<img src="/profile-pic.png" alt="profile-pic" class="max-h-40 max-w-40 rounded-full" />
-		</div>
+	<div class="md:w-1/3:flex items-center justify-center sm:block">
+		<img src="/profile-pic.png" alt="profile-pic" class="max-h-60 max-w-60 rounded-full" />
 	</div>
+</section>
+
+<section class="mb-6 flex items-center justify-center font-mono">
+	<p class="font-mono text-2xl font-bold">Some text here as well</p>
 </section>
 
 <Separator class="my-4" />
 
 <div class="mb-3 flex justify-center font-mono">Check out my socials!</div>
-<div class="flex justify-center font-mono">
+<div class="flex justify-center gap-x-3 font-mono">
 	<a href="https://github.com/vvartiainen">
 		<img
 			src="github-mark-white.png"
