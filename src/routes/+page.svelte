bb0e39382e0175e14faa1513e8f8ceccaac5ea3d
Ville Vartiainen <vvarti@gmail.com>
2024-03-02 20:06:35 +0200

    More finished skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { Separator } from '$lib/components/ui/separator';
 </script>
 
 <svelte:head>
@@ -6,18 +7,43 @@
 	<meta name="Portfolio" content="Portfolio" />
 </svelte:head>
 
-<header class="mb-10 mt-5 text-center">
-	<p class="font-mono text-5xl font-bold">Portfolio</p>
+<header class="mb-5 mt-5 gap-x-5 text-center font-mono font-bold">
+	<p class="mb-5 text-5xl font-bold">vartiainen.io</p>
+	<a href="/">Home</a>
+	<a href="/">Projects</a>
 </header>
 
-<section class="mb-10 flex justify-center gap-5">
-	<p class="font-mono">Lorem ipsum</p>
-	<p class="font-mono">Doler sit amet vai miteseoli :D</p>
+<Separator class="my-4" />
+
+<section class="mb-10 flex items-center justify-center font-mono">
+	<div class="w-2/3 gap-5 space-y-8 p-20">
+		<p>
+			Lorem ipsum dolor sit amet. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+			aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+		</p>
+		<p>
+			Moar text. Lorem ipsum dolor sit amet. Paljon on asiaa ja silleen. Excepteur sint occaecat
+			cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+			laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+			beatae vitae dicta sunt explicabo.
+		</p>
+	</div>
+	<div class="flex w-1/3 items-center justify-center">
+		<div>
+			<img src="/profile-pic.png" alt="profile-pic" class="max-h-40 max-w-40 rounded-full" />
+		</div>
+	</div>
 </section>
 
-<div class="flex justify-center">
+<Separator class="my-4" />
+
+<div class="flex justify-center font-mono">Check out my socials!</div>
+<div class="flex justify-center font-mono">
 	<a href="/">
-		<img src="/github-mark-white.png" alt="github-mark-white" class="m-1 max-w-12" />
+		<img src="github-mark-white.png" alt="github-mark-white" class="m-1 max-w-12" />
 	</a>
 
 	<a href="/">
