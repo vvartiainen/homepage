78fe36012b53dfc429f7f961ab6419dd95b4b35b
Ville Vartiainen <vvarti@gmail.com>
2024-03-03 01:58:05 +0200

    Anchor links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,17 @@
 	<meta name="Portfolio - Ville Vartiainen" content="Portfolio - Ville Vartiainen" />
 </svelte:head>
 
-<header class="mb-5 mt-10 gap-x-5 text-center font-mono font-bold">
+<header class="pl-14 pt-10 text-left font-mono font-bold">
 	<p class="mb-5 text-4xl font-bold text-sky-400">vartiainen.io</p>
 </header>
 
-<nav class="justify-left mb-5 flex pl-12">
+<div class="justify-left mb-5 flex pl-11">
 	<a href="/"><Button variant="link" class="underline-offset-8">ABOUT</Button></a>
-	<a href="/"><Button variant="link" class="underline-offset-8">TECHNOLOGIES</Button></a>
-	<a href="/"><Button variant="link" class="underline-offset-8">PROJECTS</Button></a>
-</nav>
+	<a href="#technologies">
+		<Button variant="link" class="underline-offset-8">TECHNOLOGIES</Button>
+	</a>
+	<a href="#projects"><Button variant="link" class="underline-offset-8">PROJECTS</Button></a>
+</div>
 
 <Separator class="my-4" />
 
@@ -44,28 +46,32 @@
 <Separator class="my-4" />
 
 <section class="mb-12 mt-20 flex items-center justify-center font-mono">
-	<p class="font-mono text-2xl font-bold text-sky-400">Technologies</p>
+	<p id="technologies" class="font-mono text-2xl font-bold text-sky-400">Technologies</p>
 </section>
 
 <section class="mx-auto flex items-center justify-center font-mono">
 	<div class="w-3/5 justify-center text-center">
-		<p>Technology icons here</p>
+		<p>Technology carousel here</p>
 	</div>
 </section>
 
 <section class="mb-12 mt-28 flex items-center justify-center font-mono">
-	<p class="font-mono text-2xl font-bold text-sky-400">Projects</p>
+	<p id="projects" class="font-mono text-2xl font-bold text-sky-400">Projects</p>
 </section>
 
 <section class="mx-auto flex items-center justify-center font-mono">
 	<div class="w-3/5 justify-center">
-		<p>Project description here</p>
+		<p>Project timeline here</p>
 	</div>
+</section>
+
+<section class="mb-12 mt-28 flex items-center justify-center font-mono">
+	<p class="font-mono text-2xl font-bold text-sky-400">Contact me</p>
 </section>
 
 <Separator class="my-4 mt-36" />
 
-<div class="mb-3 mt-10 flex justify-center font-mono">Check out my socials!</div>
+<div class="mb-5 mt-10 flex justify-center font-mono text-xl">Check out my socials!</div>
 <div class="mb-10 flex justify-center gap-x-3 font-mono">
 	<a href="https://github.com/vvartiainen">
 		<img
