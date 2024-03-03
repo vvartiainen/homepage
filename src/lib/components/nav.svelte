9061c4f102728b8ec71c87b5f8f7af7de7306a26
Ville Vartiainen <vvarti@gmail.com>
2024-03-04 01:04:38 +0200

    responsitivity fixes

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="mb-5 ml-6 mt-10 text-center font-mono font-extrabold md:text-left">
+<div class="mb-5 mt-10 text-center font-mono font-extrabold md:ml-6 md:text-left">
 	<p class="mb-5 ml-2 text-2xl text-sky-400 md:text-4xl">vartiainen.io</p>
 	<div class="md:flex md:text-right">
 		<a href="#about"><p class="m-3">ABOUT</p></a>
