253181dffb6bf73bce30a1b081b9417ee3245707
Ville Vartiainen <vvarti@gmail.com>
2024-03-03 16:00:08 +0200

    basic tech carousel

--- src/lib/components/tech.svelte
+++ src/lib/components/tech.svelte
@@ -1,11 +1,44 @@
+<script>
+	import * as Carousel from '$lib/components/ui/carousel';
+</script>
+
 <section class="mb-12 mt-16 flex items-center justify-center font-mono">
 	<p id="technologies" class="font-mono text-2xl font-bold text-sky-400">
-		Technologies I've used most
+		Technologies I'm most familiar with...
 	</p>
 </section>
 
-<section class="mx-auto mb-80 flex items-center justify-center pt-20 font-mono">
-	<div class="w-3/5 justify-center text-center">
-		<p>Technology carousel here</p>
-	</div>
+<section class="mb-32 mt-32 flex items-center justify-center font-mono">
+	<Carousel.Root class="w-80" opts={{ loop: true }}>
+		<Carousel.Content>
+			<Carousel.Item>
+				<div class="p-1 text-center">
+					<p class="text-2xl">NodeJS</p>
+				</div>
+			</Carousel.Item>
+			<Carousel.Item>
+				<div class="p-1 text-center">
+					<p class="text-2xl">PostgreSQL</p>
+				</div>
+			</Carousel.Item>
+			<Carousel.Item>
+				<div class="p-1 text-center">
+					<p class="text-2xl">Svelte</p>
+				</div>
+			</Carousel.Item>
+			<Carousel.Item>
+				<div class="p-1 text-center">
+					<p class="text-2xl">TailwindCSS</p>
+				</div>
+			</Carousel.Item>
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
+</section>
+
+<section class="mb-12 mt-16 flex items-center justify-center font-mono">
+	<p id="technologies" class="font-mono text-2xl font-bold text-sky-400">
+		...and many more I didn't bother to mention
+	</p>
 </section>
