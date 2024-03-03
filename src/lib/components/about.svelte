c3ccc099c0ebf8d82a80fd9394972b750db12dab
Ville Vartiainen <vvarti@gmail.com>
2024-03-03 18:13:07 +0200

    fix about responsitivity

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -1,12 +1,12 @@
-<section class="mb-10 flex items-center justify-center font-mono">
-	<div class="w-3/5 justify-center gap-5 space-y-8 p-20">
+<section class="mb-10 text-left font-mono md:flex md:items-center md:justify-center">
+	<div class="space-y-6 pl-10 pr-10 md:w-3/5 md:p-20">
 		<p>
 			My name is <span class="font-bold text-sky-400">Ville Vartiainen</span>, and I'm a technology
 			enthusiast with experience in software development and architecture.
 		</p>
 		<p>On this site you can find my professional portfolio and some personal projects.</p>
 	</div>
-	<div class="md:w-1/3:flex items-center justify-center sm:block">
+	<div class="flex items-center justify-center pt-10 md:w-1/3">
 		<img src="/profilepic.jpeg" alt="profile-pic" class="max-h-60 max-w-60 rounded-full" />
 	</div>
 </section>
