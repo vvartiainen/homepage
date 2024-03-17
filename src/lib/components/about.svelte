25420786161a9e20fc498b75dede537c738d20c2
Ville Vartiainen <vvarti@gmail.com>
2024-03-17 13:56:34 +0200

    center picture

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -1,5 +1,5 @@
 <section
-	class="mb-10 text-center font-mono md:flex md:items-center md:justify-center md:text-left md:text-xl lg:text-2xl"
+	class="mb-10 text-center font-mono md:flex items-center justify-center md:text-left md:text-xl lg:text-2xl"
 >
 	<div class="space-y-6 pl-10 pr-10 md:w-3/5 md:p-20">
 		<p>
@@ -8,7 +8,7 @@
 		</p>
 		<p>On this site you can find my professional portfolio and some personal projects.</p>
 	</div>
-	<div class="flex max-w-md justify-center pt-10 md:w-1/3">
+	<div class="flex max-w justify-center items-center pt-10 md:w-1/3">
 		<img src="profilepic.png" alt="profile-pic" class=" items-center rounded-full" />
 	</div>
 </section>
